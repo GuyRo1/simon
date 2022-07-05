9e42eedd6c722ba784bdbe33d14b16be579e9c16
GuyRo1 <Guyrossert@gmail.com>
2022-07-05 14:42:33 +0300

    removed data loader

--- App.tsx
+++ App.tsx
@@ -1,16 +1,35 @@
 import React from 'react';
-
 import {Provider} from 'react-redux';
-
 import {store, persistor} from './src/store/store';
-import Main from './Main';
 import {PersistGate} from 'redux-persist/integration/react';
-import {Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import GameScreen from './src/screens/GameScreen/GameScreen';
+import ScoreScreen from './src/screens/ScoreScreen/ScoreScreen';
+
+const stack = createNativeStackNavigator();
 
 const App = () => (
   <Provider store={store}>
-    <PersistGate loading={<Text>Loading...</Text>} persistor={persistor}>
-      <Main />
+    <PersistGate loading={null} persistor={persistor}>
+      <NavigationContainer>
+        <stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}
+          initialRouteName="main">
+          <stack.Screen
+            name="main"
+            component={GameScreen}
+            options={{title: 'Home'}}
+          />
+          <stack.Screen
+            name="topTen"
+            component={ScoreScreen}
+            options={{title: 'Best scores'}}
+          />
+        </stack.Navigator>
+      </NavigationContainer>
     </PersistGate>
   </Provider>
 );
